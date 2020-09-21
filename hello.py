e1c51bd32d281821e71c3c608300383ccbf22850
Krauno <rauno.kark@gmail.com>
2020-09-21 15:35:41 +0300

    addisin funktsiooni

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-print("What's your name?")
-name = input()
+def ask_name():
+    print("What's your name?")
+    name = input()
+    print(f"Hello, {name}!")
 
-print(f"Hello, {name}!")
+if __name__ == "__main__":
+    ask_name()
